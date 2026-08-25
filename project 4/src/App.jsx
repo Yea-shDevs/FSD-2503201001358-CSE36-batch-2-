41e63297cdcc5984e922f26abad99a66fcf6a83e
Yashvardhan Sengar <macos@Yashvardhans-MacBook-Air.local>
2026-08-25 11:22:02 +0530

    main

---
+++ project 4/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  const username = "Yash Bhardwaj";
+  let x = 10;
+  let y = 20;
+
+  return (
+    <>
+      <h1>{username}</h1>
+      <h1>{10 + 20 + 30}</h1>
+      <h1>{x + y}</h1>
+
+      <button onClick={() => alert("Hello")}>
+        Click Me
+      </button>
+    </>
+  );
+}
+
+export default App;
